4b92dad1f86cb407124959d862ccef5988e91b8c
Vaughan Oluwamayowa <vaughanoluwamayowa@gmail.com>
2025-09-04 13:10:35 +0100

    Added Global Exception Handler to handle all exceptions

--- src/main/java/com/dasvoximon/railwaysystem/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/dasvoximon/railwaysystem/exceptions/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.dasvoximon.railwaysystem.exceptions;
 
+import com.dasvoximon.railwaysystem.dto.ErrorDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
@@ -7,70 +8,121 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleGenericError(Exception ex) {
+        ErrorDto errorDto = new ErrorDto(
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(),
+                ex.getMessage()
+        );
+        return new ResponseEntity<>(errorDto, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<?> handleTypeMismatch(HttpMessageNotReadableException ex) {
-        return new ResponseEntity<>(
-                Map.of("error", "Malformed JSON or incorrect data types. " + ex.getMostSpecificCause().getMessage()),
-                HttpStatus.BAD_REQUEST
+        ErrorDto errorDto = new ErrorDto(
+                HttpStatus.BAD_REQUEST.value(),
+                HttpStatus.BAD_REQUEST.getReasonPhrase(),
+                ex.getMessage()
         );
+        return new ResponseEntity<>(errorDto, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<?> handleValidationErrors(MethodArgumentNotValidException ex) {
-        Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getFieldErrors()
-                .forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
-        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+        String message = ex.getBindingResult().getFieldErrors().stream()
+                .map(err -> err.getField() + ": " + err.getDefaultMessage())
+                .findFirst()
+                .orElse("Validation error");
+        ErrorDto errorDto = new ErrorDto(
+                HttpStatus.BAD_REQUEST.value(),
+                HttpStatus.BAD_REQUEST.getReasonPhrase(),
+                message
+        );
+        return new ResponseEntity<>(errorDto, HttpStatus.BAD_REQUEST);
     }
-
-//    @ExceptionHandler(Exception.class)
-//    public ResponseEntity<?> handleGenericError(Exception ex) {
-//        return new ResponseEntity<>(Map.of("error", "Internal Server Error"), HttpStatus.INTERNAL_SERVER_ERROR);
-//    }
 
     @ExceptionHandler(StationNotFoundException.class)
     public ResponseEntity<?> handleDoctorNotFoundException(StationNotFoundException stationNotFoundException) {
-        return new ResponseEntity<>(Map.of("error", stationNotFoundException.getMessage()), HttpStatus.NOT_FOUND);
+        ErrorDto errorDto = new ErrorDto(
+                HttpStatus.NOT_FOUND.value(),
+                HttpStatus.NOT_FOUND.getReasonPhrase(),
+                stationNotFoundException.getMessage()
+        );
+        return new ResponseEntity<>(errorDto, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(TrainNotFoundException.class)
     public ResponseEntity<?> handleTrainNotFoundException(TrainNotFoundException trainNotFoundException) {
-        return new ResponseEntity<>(Map.of("error", trainNotFoundException.getMessage()), HttpStatus.NOT_FOUND);
+        ErrorDto errorDto = new ErrorDto(
+                HttpStatus.NOT_FOUND.value(),
+                HttpStatus.NOT_FOUND.getReasonPhrase(),
+                trainNotFoundException.getMessage()
+        );
+        return new ResponseEntity<>(errorDto, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(StationConflictException.class)
     public ResponseEntity<?> handleStationConflictException(StationConflictException stationConflictException) {
-        return new ResponseEntity<>(Map.of("error", stationConflictException.getMessage()), HttpStatus.CONFLICT);
+        ErrorDto errorDto = new ErrorDto(
+                HttpStatus.NOT_FOUND.value(),
+                HttpStatus.NOT_FOUND.getReasonPhrase(),
+                stationConflictException.getMessage()
+        );
+        return new ResponseEntity<>(errorDto, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(RouteNotFoundException.class)
     public ResponseEntity<?> handleRouteNotFoundException(RouteNotFoundException routeNotFoundException) {
-        return new ResponseEntity<>(Map.of("error", routeNotFoundException.getMessage()), HttpStatus.NOT_FOUND);
+        ErrorDto errorDto = new ErrorDto(
+                HttpStatus.NOT_FOUND.value(),
+                HttpStatus.NOT_FOUND.getReasonPhrase(),
+                routeNotFoundException.getMessage()
+        );
+        return new ResponseEntity<>(errorDto, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(ScheduleNotFoundException.class)
     public ResponseEntity<?> handleScheduleNotFoundException(ScheduleNotFoundException scheduleNotFoundException) {
-        return new ResponseEntity<>(Map.of("error", scheduleNotFoundException.getMessage()), HttpStatus.NOT_FOUND);
+        ErrorDto errorDto = new ErrorDto(
+                HttpStatus.NOT_FOUND.value(),
+                HttpStatus.NOT_FOUND.getReasonPhrase(),
+                scheduleNotFoundException.getMessage()
+        );
+        return new ResponseEntity<>(errorDto, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(ReservationNotFoundException.class)
     public ResponseEntity<?> handleReservationNotFoundException(ReservationNotFoundException reservationNotFoundException) {
-        return new ResponseEntity<>(Map.of("error", reservationNotFoundException.getMessage()), HttpStatus.NOT_FOUND);
+        ErrorDto errorDto = new ErrorDto(
+                HttpStatus.NOT_FOUND.value(),
+                HttpStatus.NOT_FOUND.getReasonPhrase(),
+                reservationNotFoundException.getMessage()
+        );
+        return new ResponseEntity<>(errorDto, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(PassengerNotFoundException.class)
     public ResponseEntity<?> handlePassengerNotFoundException(PassengerNotFoundException passengerNotFoundException) {
-        return new ResponseEntity<>(Map.of("status", passengerNotFoundException.getMessage()), HttpStatus.NOT_FOUND);
+        ErrorDto errorDto = new ErrorDto(
+                HttpStatus.NOT_FOUND.value(),
+                HttpStatus.NOT_FOUND.getReasonPhrase(),
+                passengerNotFoundException.getMessage()
+        );
+        return new ResponseEntity<>(errorDto, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(SeatAlreadyTakenException.class)
     public ResponseEntity<?> handleSeatAlreadyTakenException(SeatAlreadyTakenException seatAlreadyTakenException) {
-        return new ResponseEntity<>(Map.of("error", seatAlreadyTakenException.getMessage()), HttpStatus.CONFLICT);
+        ErrorDto errorDto = new ErrorDto(
+                HttpStatus.CONFLICT.value(),
+                HttpStatus.CONFLICT.getReasonPhrase(),
+                seatAlreadyTakenException.getMessage()
+        );
+
+        return new ResponseEntity<>(errorDto, HttpStatus.CONFLICT);
     }
 }
