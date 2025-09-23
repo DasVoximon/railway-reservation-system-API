1470c3035230ba7d15c85cbafa85d96f301ac080
Vaughan Oluwamayowa <vaughanoluwamayowa@gmail.com>
2025-09-23 23:35:35 +0100

    Final Commit

--- src/main/java/com/dasvoximon/railwaysystem/repository/AdminRepository.java
+++
@@ -1,7 +0,0 @@
-package com.dasvoximon.railwaysystem.repository;
-
-import com.dasvoximon.railwaysystem.model.entity.Admin;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdminRepository extends JpaRepository<Admin, Long> {
-}
